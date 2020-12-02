c1d93dbe61f4f725a1608e860c821a16e1feffe2
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2020-12-02 22:52:26 +0900

    done :)

--- index.py
+++ index.py
@@ -42,9 +42,8 @@ contractions = {
     "you'll've": "you will have", "you're": "you are", "you've": "you have"
 }
 
+
 # 전처리 함수
-
-
 def preprocess_sentence(sentence, remove_stopwords=True):
     sentence = sentence.lower()  # 텍스트 소문자화
     # <br />, <a href = ...> 등의 html 태그 제거
@@ -77,6 +76,58 @@ def below_threshold_len(max_len, nested_list):
             cnt = cnt + 1
     print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s' %
           (max_len, (cnt / len(nested_list))))
+
+
+def decode_sequence(input_seq):
+    # 입력으로부터 인코더의 상태를 얻음
+    e_out, e_h, e_c = encoder_model.predict(input_seq)
+
+    # <SOS>에 해당하는 토큰 생성
+    target_seq = np.zeros((1, 1))
+    target_seq[0, 0] = tar_word_to_index['sostoken']
+
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:  # stop_condition이 True가 될 때까지 루프 반복
+
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + [e_out, e_h, e_c])
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_token = tar_index_to_word[sampled_token_index]
+
+        if(sampled_token != 'eostoken'):
+            decoded_sentence += ' '+sampled_token
+
+        #  <eos>에 도달하거나 최대 길이를 넘으면 중단.
+        if (sampled_token == 'eostoken' or len(decoded_sentence.split()) >= (summary_max_len-1)):
+            stop_condition = True
+
+        # 길이가 1인 타겟 시퀀스를 업데이트
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = sampled_token_index
+
+        # 상태를 업데이트 합니다.
+        e_h, e_c = h, c
+
+    return decoded_sentence
+
+
+# 원문의 정수 시퀀스를 텍스트 시퀀스로 변환
+def seq2text(input_seq):
+    temp = ''
+    for i in input_seq:
+        if(i != 0):
+            temp = temp + src_index_to_word[i]+' '
+    return temp
+
+
+# 요약문의 정수 시퀀스를 텍스트 시퀀스로 변환
+def seq2summary(input_seq):
+    temp = ''
+    for i in input_seq:
+        if((i != 0 and i != tar_word_to_index['sostoken']) and i != tar_word_to_index['eostoken']):
+            temp = temp + tar_index_to_word[i] + ' '
+    return temp
 
 
 data = pd.read_csv("my_data.csv", nrows=100000)
@@ -316,3 +367,49 @@ history = model.fit(x=[encoder_input_train, decoder_input_train], y=decoder_targ
                     validation_data=(
                         [encoder_input_test, decoder_input_test], decoder_target_test),
                     batch_size=256, callbacks=[es], epochs=50)
+
+
+src_index_to_word = src_tokenizer.index_word  # 원문 단어 집합에서 정수 -> 단어를 얻음
+tar_word_to_index = tar_tokenizer.word_index  # 요약 단어 집합에서 단어 -> 정수를 얻음
+tar_index_to_word = tar_tokenizer.index_word  # 요약 단어 집합에서 정수 -> 단어를 얻음
+
+# 인코더 설계
+encoder_model = Model(inputs=encoder_inputs, outputs=[
+                      encoder_outputs, state_h, state_c])
+
+# 이전 시점의 상태들을 저장하는 텐서
+decoder_state_input_h = Input(shape=(hidden_size,))
+decoder_state_input_c = Input(shape=(hidden_size,))
+
+dec_emb2 = dec_emb_layer(decoder_inputs)
+# 문장의 다음 단어를 예측하기 위해서 초기 상태(initial_state)를 이전 시점의 상태로 사용. 이는 뒤의 함수 decode_sequence()에 구현
+# 훈련 과정에서와 달리 LSTM의 리턴하는 은닉 상태와 셀 상태인 state_h와 state_c를 버리지 않음.
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(
+    dec_emb2, initial_state=[decoder_state_input_h, decoder_state_input_c])
+
+# 어텐션 함수
+decoder_hidden_state_input = Input(shape=(text_max_len, hidden_size))
+attn_out_inf, attn_states_inf = attn_layer(
+    [decoder_hidden_state_input, decoder_outputs2])
+decoder_inf_concat = Concatenate(
+    axis=-1, name='concat')([decoder_outputs2, attn_out_inf])
+
+# 디코더의 출력층
+decoder_outputs2 = decoder_softmax_layer(decoder_inf_concat)
+
+# 최종 디코더 모델
+decoder_model = Model(
+    [decoder_inputs] + [decoder_hidden_state_input,
+                        decoder_state_input_h, decoder_state_input_c],
+    [decoder_outputs2] + [state_h2, state_c2])
+
+file = open("result.txt", 'w')
+
+for i in range(0, 3000):
+    file.writelines("Original : "+seq2text(encoder_input_test[i])+'\n')
+    file.writelines("O summary :"+seq2summary(decoder_input_test[i])+'\n')
+    file.writelines("Pre summary :"+decode_sequence(
+        encoder_input_test[i].reshape(1, text_max_len))+'\n')
+    file.writelines('\n')
+
+file.close()
